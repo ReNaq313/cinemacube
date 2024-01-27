16aea4d79477d83048e83ffda5847be888474d83
ReNaq313 <raza77191@gmail.com>
2024-01-27 19:03:34 +0530

    remove .jsx from Img Import in Cast.jsx component & fix Img import in HeroBanner.jsx component for vercel deployment

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img.jsx";
+import Img from "../../../components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
 import { useNavigate } from "react-router-dom";
 
